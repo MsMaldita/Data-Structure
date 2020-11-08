82e21ea3acd9b0e048aab7768403a1ab5c04171f
MsMaldita <73867656+MsMaldita@users.noreply.github.com>
2020-11-08 15:02:44 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -16,17 +16,30 @@ public class Main {
         int d = sc.nextInt();
         System.out.print("What certain data type will be use?\n type 1  for integer and  2 for double ");
         int esize = sc.nextInt();
-            if (esize == 1)
-            { int dtype = 4;
-                int x[][]= new int [row][col];
-                x[row][col]=base+((row*c)+col)*esize;
-                System.out.print("Value of x for row wise:"+x);
-            }
-            else if (esize==2)
-            { int dtype = 8; }
-            else { System.out.print("Enter the correct value for data type, input either 1 or 2 only "); }
 
-            //int x[][]= new int [row][col];
+        int dtype_int = 4;
+        int k = base + ((row * c) + col) * dtype_int;
+        int s = base + (row + (col * d)) * dtype_int;
+
+        int dtype_double = 8;
+        int a = base + ((row * c) + col) * dtype_double;
+       int  j = base + (row + (col * d)) * dtype_double;
+
+
+            if (esize==1) {
+                System.out.print("Value of x for row wise:" + k);
+                System.out.print("\nValue of x for column wise:" + s);
+            }
+            else if (esize==2) {
+                System.out.print("Value of x for row wise:" + a);
+                System.out.print("\nValue of x for column wise:" + j);
+            }
+            else {
+
+                    System.out.print("Enter the correct value for data type, input either 1 or 2 only ");
+
+            }
+
         
     }
 }
